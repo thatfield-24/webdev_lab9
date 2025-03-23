14c3b85605475e406921166d51e1be82a6289c66
Tristan Hatfield <tghatfield2005@gmail.com>
2025-03-23 18:14:59 -0500

    Update script.js
    Add textAlert() function to change the h2 value and send an alert to the user

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 function init(){
-//add your javascrip between these two lines of code
+//add your javascript between these two lines of code
+ var button = document.getElementById('entrybutton');
  
-
-
-
-
-
-
+ function textAlert(){
+	 var userinput = document.getElementById('entryinput');
+	 document.getElementById('textoutput').innerHTML = userinput.value;
+	 alert("Tristan Hatfield: " + userinput.value);
+ }
+ 
+ button.addEventListener('click', textAlert)
+}
 
 window.addEventListener('load', init);
+
